64f8706add0330ece7412a1c452de618d604b08d
OtavioAugustto <otavioaugustto93@gmail.com>
2021-05-04 09:08:14 -0300

    Upload Files

---
+++ aluraframe/client/js/index.js
@@ -0,0 +1,33 @@
+var campos = [
+    document.querySelector('#data'),
+    document.querySelector('#quantidade'),
+    document.querySelector('#valor')
+];
+
+console.log(campos);
+var tBody = document.querySelector('table tbody');
+//criando tr com as tds contendo os dados dos inputs ao enviar o formulário.
+document.querySelector('.form').addEventListener('submit', function(event){
+    event.preventDefault();
+    var tr = document.createElement('tr');
+    campos.forEach(function(campos){    
+        
+        var td = document.createElement('td');
+        td.textContent = campos.value;
+        tr.appendChild(td);
+    });
+    //criando td Volume e adicionando na tr
+    var tdVolume = document.createElement('td');
+    tdVolume.textContent = campos[1].value * campos[2].value;
+    tr.appendChild(tdVolume);
+
+    //adiciona a tar na tabela! 
+    tBody.appendChild(tr);
+
+    //apagando os dados dos campos e definindo foco de preenchimento
+    campos[0].value = '';
+    campos[1].value = 1;
+    campos[2].value = 0;
+    campos[0].focus();
+    
+});
